16b36e416efdee967f6d7ea17ef9f9a4a1487d3e
chic-page-6502 <zacharytq@gmail.com>
2020-08-26 17:16:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    @reverse_name = params[:name].reverse
+    "#{@reverse_name}"
+  end
+
+  get "/square/:number" do
+    @number_square = params[:number].to_i * params[:number].to_i
+    "#{@number_square.to_s}"
+  end
+
+  get "/say/:number/:phrase" do
+    @string_to_say = params[:phrase] * params[:number].to_i
+    "#{@string_to_say}"
+  end
 
 end
